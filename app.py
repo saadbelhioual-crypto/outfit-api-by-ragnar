f35a081eb77eb2cad69c95f0e9081d34aace7c12
saadbelhioual-crypto <saadbelhioual@gmail.com>
2026-05-05 21:35:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ BACKGROUND_MODE = 'cover'           # choose 'cover' or 'contain'
 def fetch_player_info(uid: str):
     if not uid:
         return None
-    player_info_url = f"https://ragnar-host1-production.up.railway.app/proxy/JAGWAR/Keiee/player-info?uid={uid}"
+    player_info_url = f"https://info-api-by-jagwar.vercel.app/player-info?uid={uid}"
     try:
         resp = session.get(player_info_url, timeout=IMAGE_TIMEOUT)
         resp.raise_for_status()
